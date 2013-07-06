b697b1487f321199ea8ceadab1af14cd04be6f65
Maarten Billemont <lhunath@lyndir.com>
2013-07-06 12:13:07 -0400

    Guice & Webapp configuration.
    [ADDED]     Webapp initialization & Guice.

---
+++ src/main/java/com/lyndir/omnicron/model/service/UserService.java
@@ -0,0 +1,11 @@
+package com.lyndir.omnicron.model.service;
+
+
+/**
+ * <i>07 03, 2011</i>
+ *
+ * @author lhunath
+ */
+public interface UserService {
+
+}
---
+++ src/main/java/com/lyndir/omnicron/model/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lyndir.omnicron.model.service.impl;
+
+import com.lyndir.omnicron.model.service.UserService;
+
+
+/**
+ * <i>07 03, 2011</i>
+ *
+ * @author lhunath
+ */
+public class UserServiceImpl implements UserService {
+
+}
