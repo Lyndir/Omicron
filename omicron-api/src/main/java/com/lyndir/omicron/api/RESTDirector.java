89c6cbbde2f1cf46f624a45cb24829bc971ff888
Maarten Billemont <lhunath@lyndir.com>
2014-08-04 18:54:49 +0200

    Protocol buffer omicron API

---
+++ omicron-api/src/main/java/com/lyndir/omicron/api/RESTDirector.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2010, Maarten Billemont
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.lyndir.omicron.api;
+
+import com.lyndir.lhunath.opal.math.Size;
+import com.lyndir.omicron.api.model.*;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * @author lhunath, 2014-08-02
+ */
+public class RESTDirector implements Director {
+
+    @Override
+    public IGame.IBuilder gameBuilder() {
+        return new IGame.IBuilder() {
+            @Override
+            public IGame build()
+                    throws Security.NotAuthenticatedException {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public Size getLevelSize() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder setLevelSize(final Size levelSize) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public Collection<IPlayer> getPlayers() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder setPlayer(final PlayerKey playerKey, final String name, final Color primaryColor,
+                                            final Color secondaryColor) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder addPlayer(final IPlayer player) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public List<PublicVictoryConditionType> getVictoryConditions() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder addVictoryCondition(final PublicVictoryConditionType victoryCondition) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public Integer getTotalPlayers() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder setTotalPlayers(final Integer totalPlayers) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder addGameListener(final GameListener gameListener) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.GameResourceConfig getResourceConfig() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder setResourceConfig(final IGame.GameResourceConfig resourceConfig) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.GameUnitConfig getUnitConfig() {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public IGame.IBuilder setUnitConfig(final IGame.GameUnitConfig unitConfig) {
+                // TODO
+                return null;
+            }
+
+            @Override
+            public int nextPlayerID() {
+                // TODO
+                return 0;
+            }
+        };
+    }
+}
---
+++ omicron-core/src/main/java/com/lyndir/omicron/api/CoreDirector.java
@@ -0,0 +1,15 @@
+package com.lyndir.omicron.api;
+
+import com.lyndir.omicron.api.model.*;
+
+
+/**
+ * @author lhunath, 2013-08-18
+ */
+public class CoreDirector implements Director {
+
+    @Override
+    public IGame.IBuilder gameBuilder() {
+        return Game.builder();
+    }
+}
